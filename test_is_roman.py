252678a4b0860caab2103c70f8fc8df75a9d6793
Carl Pagulayan <carlpagulayan@Carls-MacBook-Pro.local>
2026-05-01 20:51:00 +0100

    Test for Roman Numerals Complete

---
+++ test_is_roman.py
@@ -0,0 +1,13 @@
+from roman_numerals import roman_numerals_converter
+
+def test_unhappy():
+    assert roman_numerals_converter(68) == "X"
+    assert roman_numerals_converter ("IX") == 9
+
+    
+    
+
+        
+        
+
+
